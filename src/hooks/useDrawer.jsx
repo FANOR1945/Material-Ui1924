160f410094e0f46720cbb3deb9cb5ab5dda4994f
FANOR <fanorarmandochoque@gmail.com>
2023-03-23 12:11:25 +0000

    solved detail programing project

--- src/hooks/useDrawer.jsx
+++ src/hooks/useDrawer.jsx
@@ -1,7 +1,30 @@
-import React, { useState } from "react";
-const useDrawer = () => {
-  const [open, setOpen] = useState(false);
-  return <useState {...open} {...setOpen(true)} />;
-};
+import React from 'react';
+import { useHistory } from 'react-router';
+export const useDrawer = () => {
+  const history = useHistory();
+  const [extended, setExtended] = React.useState(true);
 
-export default useDrawer;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleExtendOpen = () => {
+    setExtended(true);
+  };
+  const handleExtendClose = () => {
+    setExtended(false);
+  };
+
+  React.useEffect(() => {
+    history.listen(() => setMobileOpen(false));
+  }, [history]);
+  return [
+    extended,
+    mobileOpen,
+    handleDrawerToggle,
+    handleExtendClose,
+    handleExtendOpen,
+  ];
+};
