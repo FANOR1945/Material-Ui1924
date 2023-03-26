77e5d7d557569320b6ec038b1a105fa1348256c4
unknown <fanorarmandochoque@gmail.com>
2023-03-26 02:33:16 -0400

    refactor drawer

--- src/components/LandingPages/AppBar/index.jsx
+++ src/components/LandingPages/AppBar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   IconButton,
   Typography,
@@ -6,51 +6,51 @@ import {
   Box,
   Grid,
   Link,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import logoImage from "../../../assets/fonts/images/logo.png";
-import logo1Image from "../../../assets/fonts/images/logo_login.png";
-import { Content } from "../../../components/Global";
-import { useStyles } from "./app_bar.styles";
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import logoImage from '../../../assets/fonts/images/logo.png';
+import logo1Image from '../../../assets/fonts/images/logo_login.png';
+import { Content } from '../../../components/Global';
+import { useStyles } from './app_bar.styles';
 const AppBar = (props) => {
   const classes = useStyles();
   return (
     <Grid container spacing={2} className={classes.content_appbar}>
       <Grid container className={classes.griditems}>
-        <Link href="/">
-          {" "}
-          <Box component="img" src={logoImage} className={classes.image_logo} />
+        <Link href='/'>
+          {' '}
+          <Box component='img' src={logoImage} className={classes.image_logo} />
         </Link>
         <Hidden smDown>
           <Content className={classes.menu_landing_pages}>
-            <Link href="/about-us" underline="none">
+            <Link href='/about-us' underline='none'>
               <Typography className={classes.tittle}>Sobre Nosotros</Typography>
             </Link>
-            <Link href="/seeds-ours" underline="none">
+            <Link href='/seeds-ours' underline='none'>
               <Typography className={classes.tittle}>
                 Nuestras Semillas
               </Typography>
             </Link>
-            <Link href="/services-ours" underline="none">
+            <Link href='/services-ours' underline='none'>
               <Typography className={classes.tittle}>Servicios</Typography>
             </Link>
-            <Link href="/contact-us" underline="none">
+            <Link href='/contact-us' underline='none'>
               <Typography className={classes.tittle}>Contacto</Typography>
             </Link>
           </Content>
 
-          <Link href="/sign-in">
+          <Link href='/sign-in'>
             <Box
-              component="img"
+              component='img'
               src={logo1Image}
               className={classes.image_signin}
             />
           </Link>
         </Hidden>
-        <Hidden only={["md", "lg", "xl"]}>
+        <Hidden only={['md', 'lg', 'xl']}>
           <IconButton
             className={classes.icon_bottom}
-            onClick={() => props.barOpen()}
+            onClick={() => props.handleDrawerToggle()}
           >
             <MenuIcon />
           </IconButton>
--- src/layouts/PageLayout/index.jsx
+++ src/layouts/PageLayout/index.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
-import { Container } from "../../components/Global";
-import { Content } from "../../components/Global";
-import { useStyles } from "./page_layout.styles";
-import Slider from "../../components/LandingPages/Slider";
-import AppBar from "../../components/LandingPages/AppBar";
-import Footer from "../../components/LandingPages/Footer";
-import DrawerNavigation from "../../components/LandingPages/DrawerNavigation";
-import { Hidden } from "@material-ui/core";
+import React from 'react';
+import { Container } from '../../components/Global';
+import { Content } from '../../components/Global';
+import { useStyles } from './page_layout.styles';
+import Slider from '../../components/LandingPages/Slider';
+import AppBar from '../../components/LandingPages/AppBar';
+import Footer from '../../components/LandingPages/Footer';
+import DrawerNavigation from '../../components/LandingPages/DrawerNavigation';
+import { Hidden } from '@material-ui/core';
+import useDrawer from '../../hooks/useDrawer';
 
 const PageLayout = (props) => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  const barOpen = () => {
-    setOpen(!open);
-  };
+  const [mobileOpen, handleDrawerToggle] = useDrawer();
   return (
     <Container>
-      {" "}
       <Slider />
-      <AppBar barOpen={barOpen} />
+      <AppBar handleDrawerToggle={handleDrawerToggle} />
       <Hidden xlDown>
-        <DrawerNavigation variant="permanent" open={false} />
+        <DrawerNavigation variant='permanent' mobileOpen={false} />
       </Hidden>
       <Hidden xlUp>
-        <DrawerNavigation variant="temporary" open={open} onClose={barOpen} />
+        <DrawerNavigation
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+        />
       </Hidden>
       <Content className={classes.page_layout}>
         {props.children} <Footer />
-      </Content>{" "}
+      </Content>
     </Container>
   );
 };
